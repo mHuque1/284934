ccbd33e0fc2a59388c6ee1905023b205abf2b377
Mathias Huque <mathiashuque2004@gmail.com>
2024-05-08 19:00:24 -0300

    ultimos cambios parte 2

--- BusinessLogicTest/obj/Debug/net6.0/BusinessLogicTest.AssemblyInfo.cs
+++ BusinessLogicTest/obj/Debug/net6.0/BusinessLogicTest.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BusinessLogicTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e11554af3931f62e03449ce917e798f9aa487282")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35272e14d5e4d5e609fbcfb46e42d7c21b0b89de")]
 [assembly: System.Reflection.AssemblyProductAttribute("BusinessLogicTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BusinessLogicTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Interfaz/obj/Debug/net6.0/Interfaz.AssemblyInfo.cs
+++ Interfaz/obj/Debug/net6.0/Interfaz.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Interfaz")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e11554af3931f62e03449ce917e798f9aa487282")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35272e14d5e4d5e609fbcfb46e42d7c21b0b89de")]
 [assembly: System.Reflection.AssemblyProductAttribute("Interfaz")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Interfaz")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
